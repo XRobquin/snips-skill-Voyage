475ec3a2c6a02cb8e6eb8033b35436a6f2ce14ca
XRobquin <xrobquin@enseirb-matmeca.fr>
2019-08-29 23:49:40 +0200

    Update action-discussion.py

--- action-discussion.py
+++ action-discussion.py
@@ -11,6 +11,8 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 def intent_received(hermes, intent_message):
 	sentence = "Je suis ravie, j'ai toujours voulu y aller"
+	lieu = intent_message.slots.Lieu.first().value
+	sentence += lieu
 	hermes.publish_end_session(intent_message.session_id, sentence)
 
 
